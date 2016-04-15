cf2e504485eb5bc1e33b261062e104953d67b1e3
Ken Ahrens <ken.ahrens@gmail.com>
2016-04-15 16:57:41 -0400

    tweaking tests

--- test/loadserver.js
+++ test/loadserver.js
@@ -1,5 +1,8 @@
 var supertest = require('supertest');
-var server;
+
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest2');
 
 describe('loading express', function() {
   var server;
@@ -18,5 +21,18 @@ describe('loading express', function() {
     supertest(server)
       .get('/helloworld')
       .expect(200, done);
+  });
+  it('add a user', function testSlash(done) {
+    supertest(server)
+      .post('/adduser')
+      .field('fname', 'Node')
+      .field('lname', 'Js')
+      .field('userName', 'booyah')
+      .field('userEmail', 'booyah@gmail.com')
+      .field('addstreet', '100 Sunset Blvd')
+      .field('addcity', 'Beverly Hills')
+      .field('addstate', 'CA')
+      .field('addzip', '90210')
+      .expect(302, done);
   });
 })
