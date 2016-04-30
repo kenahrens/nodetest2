1755ed66337abf38cca97c3a7ead50bc06fbf17d
Ken Ahrens <ken.ahrens@gmail.com>
2016-04-30 11:35:13 -0400

    bust cache before each test case

--- test/loadserver.js
+++ test/loadserver.js
@@ -3,6 +3,7 @@ var supertest = require('supertest');
 describe('loading express', function() {
   var server;
   beforeEach(function() {
+    delete require.cache[require.resolve('../bin/www')];
     server = require('../bin/www');
   });
   afterEach(function() {
