15b0cb47b9201b24f93a1840607e028065c48ca0
Ken Ahrens <ken.ahrens@gmail.com>
2016-05-09 09:41:20 -0400

    adding search to load test

--- test/load/load.js
+++ test/load/load.js
@@ -12,8 +12,7 @@ function get(endpoint) {
   
   // Don't always issue the request, some randomness
   if (Math.random() > 0.3) {
-    request
-      .get(uri)
+    request.get(uri)
       .on('response', function(response) {
         responseCount++;
         if (response.statusCode != 200) {
@@ -33,39 +32,66 @@ function get(endpoint) {
 function addUser() {
   const faker = require('faker');
 
+  // Keep the username, we run search after the user is added
+  var username = faker.internet.userName();
+
   var uri = 'http://' + hostname + port + '/user/add';
   var form = { form: {
-      'fname': faker.name.firstName(),
-      'lname': faker.name.lastName(),
-      'username': faker.internet.userName(),
-      'useremail': faker.internet.email(),
-      'addstreet': faker.address.streetAddress(),
-      'addcity': faker.address.city(),
-      'addstate': faker.address.stateAbbr(),
-      'addzip': faker.address.zipCode()
-    }};
-  request.post(uri, form)
-    .on('response', function(response) {
-      responseCount++;
-      if (response.statusCode != 302) {
-        console.log('*** Error *** ' + response.statusCode);
-        console.log(response.body);
-      }
+    'fname': faker.name.firstName(),
+    'lname': faker.name.lastName(),
+    'username': username,
+    'useremail': faker.internet.email(),
+    'addstreet': faker.address.streetAddress(),
+    'addcity': faker.address.city(),
+    'addstate': faker.address.stateAbbr(),
+    'addzip': faker.address.zipCode()
+  }};
+
+  // Don't always issue the request, some randomness
+  if (Math.random() > 0.2) {
+    request.post(uri, form)
+      .on('response', function(response) {
+        responseCount++;
+        if (response.statusCode != 302) {
+          console.log('*** Add User Error *** ' + response.statusCode);
+          console.log(response.body);
+        } else {
+          search(username);
+        }
     });
+    requestCount++;
+  }
 }
 
 // Run the Search
-function search() {
+function search(username) {
+  var uri = 'http://' + hostname + port + '/user/runsearch';
+  var form = { form: {
+    'searchterm': username
+  }};
 
+  // Don't always issue the request, some randomness
+  if (Math.random() > 0.5) {
+
+    request.post(uri, form)
+      .on('response', function(response) {
+        responseCount++;
+        if (response.statusCode != 200) {
+          console.log('*** Search Error ***' + response.statusCode);
+          console.log(response.body);
+        }
+      });
+    requestCount++;
+  }
 }
 
 function loop() {
   addUser();
-  get('/');
-  get('/helloworld');
-  get('/user/new');
-  get('/user/list');
-  get('/user/search');
+  // get('/');
+  // get('/helloworld');
+  // get('/user/new');
+  // get('/user/list');
+  // get('/user/search');
 
   var delay = Math.random() * 10000;
   setTimeout(loop, delay);
