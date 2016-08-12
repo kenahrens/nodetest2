d578953c217850fadfb8e22417bd809d799edada
Ken Ahrens <ken.ahrens@gmail.com>
2016-08-12 16:06:53 -0400

    updating packages and tweaking profiler data

--- profilers.js
+++ profilers.js
@@ -3,8 +3,11 @@ var profiler = require('gc-profiler');
 
 // Memory profiling via custom events
 profiler.on('gc', function (info) {
-  newrelic.recordMetric('Custom/GC', info.duration / 1000)
-  newrelic.recordCustomEvent('GC', info)
+  info.pid = process.pid;
+  info.timestamp = info.date.getTime();
+  delete info.date;
+  newrelic.recordMetric('Custom/GC/' + info.type, info.duration / 1000);
+  newrelic.recordCustomEvent('GC', info);
 });
 console.log('Memory Profiler Enabled');
 
@@ -15,21 +18,23 @@ if (process.cpuUsage) {
   console.log('CPU Profiler Enabled');
   var cpuPoller = pollCpu(newrelic.agent);
   cpuPoller();
-  setInterval(cpuPoller, 30000);
+  // setInterval(cpuPoller, 5000);
+  setInterval(cpuPoller, 60000);
 } else {
-  console.log('CPU usage available only in Node 6+, this is running: ' + process.version);
+  console.log('CPU usage available only in Node 6.1/0+, this is running: ' + process.version);
 }
 
 // Helper function to get the CPU Usage
+var lastCpuValue;
 function pollCpu(agent) {
-  var lastSample;
   return function cpuSampler() {
     try {
-      var cpu = process.cpuUsage(lastSample);
+      var cpu = process.cpuUsage(lastCpuValue);
       var userCpuUsageInSeconds = cpu.user / 1e6;
       var stats = agent.metrics.getOrCreateMetric('CPU/User Time');
       stats.recordValue(userCpuUsageInSeconds);
-      lastSample = cpu;
+      // console.log('Recording CPU value: ' + userCpuUsageInSeconds);
+      lastCpuValue = cpu;
     } catch (e) {
       console.log(e);
     }
